b40ce1166217bc1e270b6f03ec4ac6db78944cee
shcaba <jason.cope@noaa.gov>
2025-06-26 14:09:46 -0700

    viz plot code

--- Document/report/plots_4_doc/Page1_modoutput_plot.R
+++ Document/report/plots_4_doc/Page1_modoutput_plot.R
@@ -8,9 +8,9 @@ Model_2025<-subset(assessment_comps_1325,Model=="2025")
 #pal <- wes_palette("AsteroidCity3", 100, type = "continuous")
 pal <- viridis(100)
 ggplot(assessment_comps_1325,aes(x=Year))+
+  geom_line(aes(y = Depletion_line,group=Model),col="black",lwd=1.1)+
   geom_point(aes(y = Depletion,colour=Scale_comp))+
   geom_ribbon(aes(x = Year,ymin = Lowdep, ymax = Hidep,group=Model),fill = "gray30", alpha = 0.2)+
-  geom_line(aes(y = Depletion_line,group="Model"),col="black")+
   ylim(0,1.2)+
   labs(colour="Absolute abundance multiplier of current vs past model values")+
   geom_hline(yintercept = c(0.4,0.25),col=c("black","red"),linetype = c("dashed","solid"))+
@@ -22,7 +22,7 @@ ggplot(assessment_comps_1325,aes(x=Year))+
   guides(colour = guide_colorbar(title.position = "top"))+
   geom_line(aes(x = Year,y = Catch_stand),col=viridis(1))+
   annotate(geom="text",x=c(1905,1903),y=c(0.45,0.21),label=c("Target stock status","Limit stock status"),color=c("black","red"))+
-  annotate(geom="text",x=c(2013),y=c(0.07),label=c("Catch history"),color=col=viridis(1))
+  annotate(geom="text",x=c(2013),y=c(0.07),label=c("Catch history"),color=viridis(1))
 
   
   

518236caa2ba03b1687feb7fb0f93283d5f1f809
Claire Rosemond <claire.rosemond@noaa.gov>
2025-07-17 07:40:46 -0700

    1980 rec dev model

--- Rcode/review_request_8ish_recdev.R
+++ Rcode/review_request_8ish_recdev.R
@@ -51,6 +51,57 @@ SSplotComparisons(models_summary,
                   legendloc = "bottomright"
 )
 
+###################################################
+
+#1980 model
+
+base_model_dir <- here::here(
+  'Document',
+  'report',
+  'ref_model'
+)
+
+base_model <- r4ss::SS_read(base_model_dir, ss_new = TRUE)
+#base_out <- r4ss::SS_output(base_model_dir)
+
+recdev1980_model <- base_model
+
+recdev1980_model$start$init_values_src <- 0
+
+recdev1980_model_dir <- here::here(
+  'models',
+  'recdev1980_model'
+)
+
+ctlfile <- recdev1980_model$ctl
+ctlfile$MainRdevYrFirst <- 1980
+
+recdev1980_model$ctl <- ctlfile
+
+r4ss::SS_write(recdev1980_model, dir = recdev1980_model_dir, overwrite = TRUE)
+
+r4ss::get_ss3_exe(dir = recdev1980_model_dir)
+
+#r4ss::run(dir = recdev1920_model_dir, show_in_console = TRUE, extras = "-nohess")
+r4ss::run(dir = recdev1980_model_dir, show_in_console = TRUE)
+
+replist <- SS_output(dir = recdev1980_model_dir)
+SS_plots(replist)
+
+####################################################
+models <- c(base_model_dir, recdev1980_model_dir)
+models
+models_output <- SSgetoutput(dirvec = models)
+models_summary <- SSsummarize(models_output)
+SSplotComparisons(models_summary,
+                  plotdir = here::here("Rcode/SSplotComparisons_output/review/high_state"),
+                  legendlabels = c("2025 base model", "recdevs first year 1980"),
+                  print = TRUE,
+                  legendloc = "bottomright"
+)
+
+
+
 ###########################################
 retro(
   dir = recdev1920_model_dir, # wherever the model files are

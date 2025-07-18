fe019941809fb3ed3c705f0b51237c669b0cf401
Claire Rosemond <claire.rosemond@noaa.gov>
2025-07-18 13:18:56 -0700

    update decision table function

--- Rcode/decision_table.R
+++ Rcode/decision_table.R
@@ -1,3 +1,58 @@
+#' Extract total catch, spawning output, and fraction unfished from forecast years
+#'
+#' Values of total catch, spawning output, and fraction unfished are extracted
+#' from the forecast years of a time series table for inclusion in a decision table.
+#'
+#' @template replist
+#' @param yrs Range of years from which to extract values
+#' @param digits Vector of number of digits to round to in table for
+#' \itemize{
+#'   \item 1 catch
+#'   \item 2 spawning output
+#'   \item 3 fraction unfished (column is called "depl")
+#' }
+#' @export
+#' @return A tibble with columns for year, total catch (dead biomass),
+#' spawning output, and fraction unfished.
+#' @seealso [SS_ForeCatch()]
+#' @author Ian G. Taylor
+#' @export
+
+SS_decision_table_stuff2 <- function(
+    replist,
+    yrs = 2025:2036,
+    digits = c(0, 0, 3)
+) {
+  unfished <- replist[["derived_quants"]]["SSB_Virgin", "Value"]
+  replist[["timeseries"]] |>
+    dplyr::filter(Yr %in% yrs) |>
+    dplyr::group_by(Yr) |>
+    dplyr::summarise(
+      catch = sum(rowSums(
+        dplyr::across(dplyr::starts_with("dead(B)")),
+        na.rm = TRUE
+      )),
+      spawn_bio = sum(SpawnBio, na.rm = TRUE)
+    ) |>
+#    dplyr::mutate(
+      # calculation of fraction_unfished is independent of Bratio definition
+      # which could have a different denominator
+#      fraction_unfished = spawn_bio / unfished
+#    ) |>
+    dplyr::mutate(
+      catch = round(catch, digits[1]),
+      spawn_bio = round(spawn_bio, digits[2]),
+      fraction_unfished = round(fraction_unfished, digits[3])
+    ) |>
+    dplyr::rename(
+      # matching names in earlier version of the function for backwards compatibility
+      yr = Yr,
+      SpawnBio = spawn_bio,
+      dep = fraction_unfished
+    )
+}
+
+
 #' Format a decision table
 #'
 #' Format a decision table for its inclusion in a document.
@@ -50,7 +105,7 @@ table_decision <- function(
   results <- purrr::modify_depth(
     mods,
     .depth = 2,
-    .f = r4ss::SS_decision_table_stuff,
+    .f = SS_decision_table_stuff2,
     yrs = years,
     digits = digits
   ) |>
